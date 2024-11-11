c884ce8f54e58785c369c530acb5b4dc5858dd7f
Steve Desmond <Steve@ecoAPM.com>
2024-11-11 15:50:59 -0500

    Add GitHub API GraphQL proof-of-concept scaffolding

--- index.ts
+++ index.ts
@@ -1,2 +1,36 @@
-window.onload = () => {
+const newToken = import.meta.env.VITE_GITHUB_API_TOKEN;
+const data = `query {
+	node(id:"PVT_kwDOBbNjPc4Ad9Mg") {
+		... on ProjectV2 {
+			items(first:100) {
+				nodes {
+					content {
+						... on Issue {
+							number
+							title
+							body
+						}
+					}
+					fieldValues(first:5) {
+						nodes {
+							... on ProjectV2ItemFieldDateValue {
+								date
+							}
+							... on ProjectV2ItemFieldSingleSelectValue {
+								name
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}`;
+const authHeaders = { 'Authorization': 'Bearer ' + newToken };
+const body = JSON.stringify({ query: data });
+
+window.onload = async () => {
+	const response = await fetch('https://api.github.com/graphql', { method: 'POST', headers: authHeaders, body: body });
+	const json = await response.json();
+	console.log(json);
 };
