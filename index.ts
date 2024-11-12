b88bb3cad13100b1fd7e80896f4778c5342f0147
Steve Desmond <Steve@ecoAPM.com>
2024-11-11 19:07:20 -0500

    Add changes from our meetup

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import dompurify from 'dompurify';
+
 const newToken = import.meta.env.VITE_GITHUB_API_TOKEN;
 const data = `query {
 	node(id:"PVT_kwDOBbNjPc4Ad9Mg") {
@@ -8,7 +10,7 @@ const data = `query {
 						... on Issue {
 							number
 							title
-							body
+							bodyHTML
 						}
 					}
 					fieldValues(first:5) {
@@ -29,8 +31,28 @@ const data = `query {
 const authHeaders = { 'Authorization': 'Bearer ' + newToken };
 const body = JSON.stringify({ query: data });
 
+const renderEvent = (event) => {
+	return `<h3>${dompurify.sanitize(event.title)}</h3>
+			<h4>${dompurify.sanitize(event.date)}</h4>
+			${dompurify.sanitize(event.description)}`;
+}
+
+const getEvents = (json) => json.data.node.items.nodes.map(i => {
+	const fields = Object.assign({}, ...i.fieldValues.nodes);
+	return {
+		id: i.content.number,
+		title: i.content.title,
+		description: i.content.bodyHTML,
+		status: fields.name,
+		date: fields.date
+	};
+}).filter(i => i.status === 'Planned' && i.date);
+
 window.onload = async () => {
 	const response = await fetch('https://api.github.com/graphql', { method: 'POST', headers: authHeaders, body: body });
 	const json = await response.json();
+	const events = getEvents(json);
+	const rootElement = document.getElementById('upcoming-events');
+	rootElement.innerHTML = `${events.map(renderEvent).join('')}`; 
 	console.log(json);
 };
